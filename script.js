c14c32d4449d0aad194b5035bdf3818d399273ab
Felipe C Santos <felipecsantos777@gmail.com>
2024-08-01 16:08:17 -0300

    versao_01.08.2024

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const form = document.getElementById('contact-form');
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    const name = form.elements['name'].value;
+    const name = form.elements['nome'].value;
     const email = form.elements['email'].value;
-    const message = form.elements['message'].value;
+    const message = form.elements['mensagem'].value;
     
     const result = `Nome: ${name}\nE-mail: ${email}\nMessage: ${message}`;
     alert(result);
